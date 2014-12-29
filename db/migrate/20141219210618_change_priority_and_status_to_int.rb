b2309cf237978e8e28fdf7d26c13730870bdceac
Jimmy <jimmylorunning@gmail.com>
2014-12-28 19:19:33 -0500

    Fix migration for pg

--- db/migrate/20141219210618_change_priority_and_status_to_int.rb
+++ db/migrate/20141219210618_change_priority_and_status_to_int.rb
@@ -1,11 +1,11 @@
 class ChangePriorityAndStatusToInt < ActiveRecord::Migration
   def up
-  	change_column :todos, :priority, :integer
-   	change_column :todos, :status, :integer
+  	change_column :todos, :priority, 'integer USING CAST(priority AS integer)'
+   	change_column :todos, :status, 'integer USING CAST(status AS integer)'
   end
 
   def down
-  	change_column :todos, :priority, :string
-  	change_column :todos, :status, :integer
+#  	change_column :todos, :priority, :string
+#  	change_column :todos, :status, :string
   end
 end
